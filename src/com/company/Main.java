61df0d1666e0c4c4bb7c287788a4d08a70553108
Asya Mamo <asyamamo96@gmail.com>
2017-01-27 00:23:20 +0300

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int x=10;
+	int y=20;
+	int result1;
+        int result2;
+        int result3;
+        int result4;
+        int result5;
+        result1=x+y;
+        System.out.println("x+y="+result1);
+        result2=x-y;
+        System.out.println("x-y="+result2);
+	result3=x*y;
+        System.out.println("x*y="+result3);
+	result4=x/y;
+        System.out.println("x/y="+result4);
+	result5=x%3;
+	System.out.println("x%3="+result5);
+
+    }
+}
